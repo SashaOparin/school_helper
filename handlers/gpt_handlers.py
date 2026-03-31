df4c477d606e892fc05746f12f2833b62542627d
SashaOparin <aleksandroparin2013@yandex.ru>
2026-03-31 19:02:59 +0700

    НУ ДУМАЮ ГОТОВО

--- handlers/gpt_handlers.py
+++ handlers/gpt_handlers.py
@@ -23,7 +23,7 @@ async def start_gpt(update: Update, context: ContextTypes.DEFAULT_TYPE):
     markup = InlineKeyboardMarkup(keyboard)
 
     await query.edit_message_text(
-        text="Вы зашли в ассистента.  вопрос.",
+        text="Вы зашли в ассистента. Задайтевопрос.",
         reply_markup=markup,
     )
     return GPT
